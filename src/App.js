9f3f5376b1f6b8b783a5ef81b65e0d36e253d644
NazarFedus <simpleprograming7@gmail.com>
2023-04-05 13:37:16 +0100

    add styles for Navbar, add Header component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import linkedIn from './img/icons/linkedIn.svg'
 
 //components
 import { Navbar } from './components/navbar/Navbar'
+import { Header } from './components/header/Header'
+
 
 
 function App() {
@@ -15,18 +17,7 @@ function App() {
     <div className="App">
         <Navbar></Navbar>
 
-        <header className="header">
-        <div className="header__wrapper">
-            <h1 className="header__title">
-                <strong>Hi, my name is <em>Yuri</em></strong><br />
-                a frontend developer
-            </h1>
-            <div className="header__text">
-                <p>with passion for learning and creating.</p>
-            </div>
-            <a href="#!" className="btn">Download CV</a>
-        </div>
-        </header>
+        <Header></Header>
 
         <main className="section">
         <div className="container">
