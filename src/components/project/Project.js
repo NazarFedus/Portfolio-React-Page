048af467fa3738775047686b231341142bb8c532
NazarFedus <simpleprograming7@gmail.com>
2023-04-06 12:47:27 +0100

    add logic to select project in Projects page

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,12 +1,14 @@
+import { NavLink } from 'react-router-dom';
 import './style.css'
+
 
 export function Project({project}){
      return (
-          <li className="project">
-               <a href="./project-page.html">
-                    <img src={project.img} alt={project.title} className="project__img" />
-                    <h3 className="project__title">{project.title}</h3>
-               </a>
-          </li>
+          <NavLink to={`/project/${project.id}`}>
+               <li className="project">
+                         <img src={project.img} alt={project.title} className="project__img" />
+                         <h3 className="project__title">{project.title}</h3>
+               </li>
+          </NavLink>
      );
 }
